35901874ae33bb7a1a57a1ba6bb4d86dc9cdfcd6
aryananand-04 <aryananand.dev04@gmail.com>
2025-12-02 23:43:16 +0530

    Actions Fix 2

--- components/Achievements.tsx
+++ components/Achievements.tsx
@@ -53,12 +53,6 @@ export default function Achievements() {
                 </div>
               </div>
 
-              {/* Ranking */}
-              <div className="bg-gradient-to-r from-orange-500 to-red-500 text-white rounded-lg p-4 text-center">
-                <div className="text-sm opacity-90">Ranking</div>
-                <div className="text-2xl font-bold">{achievementsData.coc.ranking}</div>
-              </div>
-
               {/* Screenshots */}
               <div className="grid grid-cols-2 gap-2">
                 {achievementsData.coc.screenshots.map((screenshot, idx) => (
